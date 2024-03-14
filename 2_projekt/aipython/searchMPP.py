f069ea5259b74ff8aa46507289e980a945085911
Olgierd Piofczyk <olgierdpk@gmail.com>
2024-03-14 12:36:25 +0100

    gowno gowno gowno

--- 2_projekt/aipython/searchMPP.py
+++ 2_projekt/aipython/searchMPP.py
@@ -8,20 +8,24 @@
 # Attribution-NonCommercial-ShareAlike 4.0 International License.
 # See: https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
 
+from searchGeneric import test
+import searchExample
 from searchGeneric import AStarSearcher
 from searchProblem import Path
+
 
 class SearcherMPP(AStarSearcher):
     """returns a searcher for a problem.
     Paths can be found by repeatedly calling search().
     """
+
     def __init__(self, problem):
         super().__init__(problem)
         self.explored = set()
 
     def search(self):
         """returns next path from an element of problem's start nodes
-        to a goal node. 
+        to a goal node.
         Returns None if no path exists.
         """
         while not self.empty_frontier():
@@ -32,24 +36,25 @@ class SearcherMPP(AStarSearcher):
                 if self.problem.is_goal(self.path.end()):
                     self.solution = self.path   # store the solution found
                     self.display(1, f"Solution: {self.path} (cost: {self.path.cost})\n",
-                    self.num_expanded, "paths have been expanded and",
-                            len(self.frontier), "paths remain in the frontier")
+                                 self.num_expanded, "paths have been expanded and",
+                                 len(self.frontier), "paths remain in the frontier")
                     return self.path
                 else:
-                    self.display(4,f"Expanding: {self.path} (cost: {self.path.cost})")
+                    self.display(
+                        4, f"Expanding: {self.path} (cost: {self.path.cost})")
                     neighs = self.problem.neighbors(self.path.end())
-                    self.display(2,f"Expanding: {self.path} with neighbors {neighs}")
+                    self.display(
+                        2, f"Expanding: {self.path} with neighbors {neighs}")
                     for arc in neighs:
-                        self.add_to_frontier(Path(self.path,arc))
-                    self.display(3, f"New frontier: {[p.end() for p in self.frontier]}")
-        self.display(0,"No (more) solutions. Total of",
-                     self.num_expanded,"paths expanded.")
+                        self.add_to_frontier(Path(self.path, arc))
+                    self.display(
+                        3, f"New frontier: {[p.end() for p in self.frontier]}")
+        self.display(0, "No (more) solutions. Total of",
+                     self.num_expanded, "paths expanded.")
 
-from searchGeneric import test
+
 if __name__ == "__main__":
     test(SearcherMPP)
 
-import searchExample
 # searcherMPPcdp = SearcherMPP(searchExample.cyclic_simp_delivery_graph)
 # searcherMPPcdp.search()  # find first path
-
