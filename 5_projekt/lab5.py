a5b861bd3d5fbb6eb65b6102cf7faf5a81c23d7a
Olgierd Piofczyk <piofczyk@student.agh.edu.pl>
2024-05-16 11:52:08 +0200

    a

---
+++ 5_projekt/lab5.py
@@ -0,0 +1,10 @@
+import gymnasium as gym
+
+env = gym.make("LunarLander-v2",
+    continuous = False,
+    gravity = -10.0,
+    enable_wind = False,
+    wind_power = 15.0,
+    turbulence_power = 1.5,
+)
+
